a0bbbd1ba37ea7b6c5893bd5beb7ef2e919dfcb5
hamogu <hgunther@mit.edu>
2016-06-02 16:19:59 -0400

    Split analysis file and arcus definition file

---
+++ arcus.py
@@ -0,0 +1,76 @@
+import numpy as np
+import xlrd
+import astropy.units as u
+from scipy.interpolate import interp1d
+import transforms3d
+
+import marxs
+import marxs.optics
+from marxs.simulator import Sequence
+from marxs.optics import (GlobalEnergyFilter, EnergyFilter,
+                          FlatDetector, CATGrating)
+from marxs.design.rowland import (RowlandTorus, design_tilted_torus,
+                                  GratingArrayStructure, LinearCCDArray)
+
+from read_grating_data import InterpolateRalfTable
+
+# Reading in data for grating reflectivity, filtercurves etc.
+arcusefficiencytable = xlrd.open_workbook('../ArcusEffectiveArea-v3.xls')
+mastersheet = arcusefficiencytable.sheet_by_name('Master')
+energy = np.array(mastersheet.col_values(0, start_rowx=6)) / 1000.  # ev to keV
+spogeometricthroughput = np.array(mastersheet.col_values(3, start_rowx=6))
+doublereflectivity = np.array(mastersheet.col_values(4, start_rowx=6))
+sifiltercurve = np.array(mastersheet.col_values(20, start_rowx=6))
+uvblocking = np.array(mastersheet.col_values(21, start_rowx=6))
+opticalblocking = np.array(mastersheet.col_values(22, start_rowx=6))
+ccdcontam = np.array(mastersheet.col_values(23, start_rowx=6))
+qebiccd = np.array(mastersheet.col_values(24, start_rowx=6))
+
+
+mirrorefficiency = GlobalEnergyFilter(filterfunc=interp1d(energy, spogeometricthroughput * doublereflectivity))
+
+entrancepos = np.array([12000., 0., 0.])
+
+# Set a little above entrance pos (the mirror) for display purposes.
+# Thus, needs to be geometrically bigger for off-axis sources.
+
+aper = marxs.optics.CircleAperture(position=[12200, 0, 0], zoom=300)
+lens = marxs.optics.PerfectLens(focallength=12000., position=entrancepos)
+rms = marxs.optics.RadialMirrorScatter(inplanescatter=(24 * u.arcsec).to(u.radian).value,
+                                       perpplanescatter=(1.05 * u.arcsec).to(u.radian).value,
+                                       position=entrancepos)
+
+mirror = Sequence(elements=[lens, rms, mirrorefficiency])
+
+# CAT grating
+order_selector = InterpolateRalfTable('../Si_4um_deep_30pct_dc.xlsx')
+
+# Define L1, L2 blockage as simple filters due to geometric area
+# L1 support: blocks 18 %
+# L2 support: blocks 19 %
+catsupport = GlobalEnergyFilter(filterfunc=lambda e: 0.81 * 0.82)
+
+blazeang = 1.91
+R, r, pos4d = design_tilted_torus(12e3, np.deg2rad(blazeang),
+                                  2 * np.deg2rad(blazeang))
+rowland = RowlandTorus(R, r, pos4d=pos4d)
+blazemat = transforms3d.axangles.axangle2mat(np.array([0, 1, 0]), np.deg2rad(blazeang))
+gas = GratingArrayStructure(rowland=rowland, d_element=30.,
+                            x_range=[1e4, 1.4e4],
+                            radius=[50, 300], phi=[0, 2. * np.pi],
+                            elem_class=CATGrating,
+                            elem_args={'d': 2e-4, 'zoom': [1., 10., 10.], 'orientation': blazemat,
+                                       'order_selector': order_selector},
+                        )
+
+
+flatstackargs = {'zoom': [1, 24.576, 12.288],
+                 'elements': [EnergyFilter, FlatDetector],
+                 'keywords': [{'filterfunc': interp1d(energy, sifiltercurve * uvblocking * opticalblocking * ccdcontam * qebiccd)}, {'pixsize': 0.024}]
+                 }
+# 500 mu gap between detectors
+det = LinearCCDArray(rowland=rowland, elem_class=marxs.optics.FlatStack,
+                     elem_args=flatstackargs, d_element=49.652, phi=0,
+                     x_range=[0, 200], radius=[-1000, -400])
+
+arcus = Sequence(elements=[aper, mirror, gas, catsupport, det])
--- raytrace.py
+++ raytrace.py
@@ -1,101 +1,36 @@
 import numpy as np
-import xlrd
-import astropy.units as u
-from scipy.interpolate import interp1d
-import transforms3d
-
 import marxs
 import marxs.optics
 from marxs.simulator import Sequence
 from marxs.source import PointSource, FixedPointing
-from marxs.optics import (GlobalEnergyFilter, EnergyFilter,
-                          FlatDetector, CATGrating)
-from marxs.design.rowland import (RowlandTorus, design_tilted_torus,
-                                  GratingArrayStructure, LinearCCDArray)
+from marxs.analysis import fwhm_per_order, weighted_per_order
 
-from read_grating_data import InterpolateRalfTable
-
-# Reading in data for grating reflectivity, filtercurves etc.
-arcusefficiencytable = xlrd.open_workbook('../ArcusEffectiveArea-v3.xls')
-mastersheet = arcusefficiencytable.sheet_by_name('Master')
-energy = np.array(mastersheet.col_values(0, start_rowx=6)) / 1000.  # ev to keV
-spogeometricthroughput = np.array(mastersheet.col_values(3, start_rowx=6))
-doublereflectivity = np.array(mastersheet.col_values(4, start_rowx=6))
-sifiltercurve = np.array(mastersheet.col_values(20, start_rowx=6))
-uvblocking = np.array(mastersheet.col_values(21, start_rowx=6))
-opticalblocking = np.array(mastersheet.col_values(22, start_rowx=6))
-ccdcontam = np.array(mastersheet.col_values(23, start_rowx=6))
-qebiccd = np.array(mastersheet.col_values(24, start_rowx=6))
+from mayavi import mlab
+from marxs.math.pluecker import h2e
+import marxs.visualization.mayavi
 
 
-mirrorefficiency = GlobalEnergyFilter(filterfunc=interp1d(energy, spogeometricthroughput * doublereflectivity))
-
-
-pointing = FixedPointing(coords=(23., 45.), roll=0.)
-entrancepos = np.array([12000., 0., 0.])
-
-# Set a little above entrance pos (the mirror) for display purposes.
-# Thus, needs to be geometrically bigger for off-axis sources.
-
-aper = marxs.optics.CircleAperture(position=[12200, 0, 0], zoom=300)
-lens = marxs.optics.PerfectLens(focallength=12000., position=entrancepos)
-rms = marxs.optics.RadialMirrorScatter(inplanescatter=(24 * u.arcsec).to(u.radian).value,
-                                       perpplanescatter=(1.05 * u.arcsec).to(u.radian).value,
-                                       position=entrancepos)
-
-mirror = Sequence(elements=[lens, rms, mirrorefficiency])
-
-# CAT grating
-order_selector = InterpolateRalfTable('../Si_4um_deep_30pct_dc.xlsx')
-
-# Define L1, L2 blockage as simple filters due to geometric area
-# L1 support: blocks 18 %
-# L2 support: blocks 19 %
-catsupport = GlobalEnergyFilter(filterfunc=lambda e: 0.81 * 0.82)
-
-blazeang = 1.91
-R, r, pos4d = design_tilted_torus(12e3, np.deg2rad(blazeang),
-                                                 2 * np.deg2rad(blazeang))
-rowland = RowlandTorus(R, r, pos4d=pos4d)
-blazemat = transforms3d.axangles.axangle2mat(np.array([0, 1, 0]), np.deg2rad(blazeang))
-mytorus = RowlandTorus(R, r, pos4d=pos4d)
-gas = GratingArrayStructure(rowland=rowland, d_element=22., x_range=[1e4, 1.4e4],
-                            radius=[50, 300], phi=[0, 2. * np.pi],
-                            elem_class=CATGrating,
-                            elem_args={'d': 2e-4, 'zoom': [1., 10., 10.], 'orientation': blazemat,
-                                       'order_selector': order_selector},
-                            )
-
-
-flatstackargs = {'zoom': [1, 24.576, 12.288],
-                 'elements': [EnergyFilter, FlatDetector],
-                 'keywords': [{'filterfunc': interp1d(energy, sifiltercurve * uvblocking * opticalblocking * ccdcontam * qebiccd)}, {'pixsize': 0.024}]
-                 }
-# 500 mu gap between detectors
-det = LinearCCDArray(rowland=mytorus, elem_class=marxs.optics.FlatStack,
-                     elem_args=flatstackargs, d_element=49.652, phi=0,
-                     x_range=[0, 200], radius=[-1000, -400])
+from .arcus import rowland, aper, mirror, gas, catsupport, det
 
 # Place an additional detector in the focal plane for comparison
 # Detectors are transparent to allow this stuff
-detfp = FlatDetector(zoom=[.2, 1000, 1000])
+detfp = marxs.optics.FlatDetector(zoom=[.2, 1000, 1000])
 detfp.loc_coos_name = ['detfp_x', 'detfp_y']
 detfp.detpix_name = ['detfppix_x', 'detfppix_y']
 detfp.display['opacity'] = 0.2
 
 
 keeppos = marxs.simulator.KeepCol('pos')
-arcus = Sequence(elements=[aper, mirror, gas, catsupport, det, detfp], postprocess_steps=[keeppos])
+arcus = Sequence(elements=[aper, mirror, gas, catsupport, det, detfp],
+                 postprocess_steps=[keeppos])
 
 star = PointSource(coords=(23., 45.), flux=5.)
+pointing = FixedPointing(coords=(23., 45.))
 photons = star.generate_photons(exposuretime=200)
 p = pointing(photons)
 p = arcus(p)
 
 
-from mayavi import mlab
-from marxs.math.pluecker import h2e
-import marxs.visualization.mayavi
 fig = mlab.figure()
 mlab.clf()
 
@@ -106,7 +41,5 @@ d = h2e(d)
 marxs.visualization.mayavi.plot_rays(d, viewer=fig)
 arcus.plot(format="mayavi", viewer=fig)
 
-theta, phi = np.mgrid[-0.2+np.pi:0.2+np.pi:60j, -1:1:60j]
-mytorus.plot(theta=theta, phi=phi, viewer=fig, format='mayavi')
-
-from marxs.analysis import fwhm_per_order, weighted_per_order
+theta, phi = np.mgrid[-0.2 + np.pi:0.2 + np.pi:60j, -1:1:60j]
+rowland.plot(theta=theta, phi=phi, viewer=fig, format='mayavi')
