ad8d281f46cdb7bdd29c649db34acee1efef4aa8
hamogu <hgunther@mit.edu>
2016-11-01 17:01:42 -0400

    Shift the two pairs of channels in z to sparate them on detector

--- arcus/arcus.py
+++ arcus/arcus.py
@@ -33,25 +33,47 @@ opticalblocking = np.array(mastersheet.col_values(22, start_rowx=6))
 ccdcontam = np.array(mastersheet.col_values(23, start_rowx=6))
 qebiccd = np.array(mastersheet.col_values(24, start_rowx=6))
 
-
+# Blaze angle in degrees
 blazeang = 1.91
+
+# We want to different spectral traces in the same array of chips.
+# Offset them a little so that the spectra can be separated easily in data analysis.
+# CCDs are ~25 mm wide.
+z_offset_spectra = 5.
 
 alpha = np.deg2rad(2 * blazeang)
 beta = np.deg2rad(4 * blazeang)
 R, r, pos4d = design_tilted_torus(12e3, alpha, beta)
+rowland_central = RowlandTorus(R, r, pos4d=pos4d)
+
+# Now offset that Rowland torus in a z axis by a few mm.
+# Shift is measured from a focal point that hits the center of the CCD strip.
+shift_optical_axis_1 = np.eye(4)
+shift_optical_axis_1[2, 3] = - z_offset_spectra
+
 rowland = RowlandTorus(R, r, pos4d=pos4d)
+rowland.pos4d = np.dot(shift_optical_axis_1, rowland.pos4d)
+
 
 Rm, rm, pos4dm = design_tilted_torus(12e3, - alpha, -beta)
 rowlandm = RowlandTorus(Rm, rm, pos4d=pos4dm)
 d = r * np.sin(alpha)
-shift_optical_axis = np.eye(4)
-shift_optical_axis[1, 3] = 2. * d
-rowlandm.pos4d = np.dot(shift_optical_axis, rowlandm.pos4d)
+# Relative to z=0 in the center of the CCD strip
+shift_optical_axis_2 = np.eye(4)
+shift_optical_axis_2[1, 3] = 2. * d
+shift_optical_axis_2[2, 3] = + z_offset_spectra
+
+# Relative to optical axis 1
+shift_optical_axis_12 = np.eye(4)
+shift_optical_axis_12[1, 3] = 2. * d
+shift_optical_axis_12[2, 3] = 2.* z_offset_spectra
+
+rowlandm.pos4d = np.dot(shift_optical_axis_2, rowlandm.pos4d)
 
 
 mirrorefficiency = GlobalEnergyFilter(filterfunc=interp1d(energy, spogeometricthroughput * doublereflectivity))
 
-entrancepos = np.array([12000., 0., 0.])
+entrancepos = np.array([12000., 0., -z_offset_spectra])
 
 # Set a little above entrance pos (the mirror) for display purposes.
 # Thus, needs to be geometrically bigger for off-axis sources.
@@ -62,14 +84,14 @@ entrancepos = np.array([12000., 0., 0.])
 aper_rect1 = optics.RectangleAperture(position=[12200, 0, 550], zoom=[1, 180, 250])
 aper_rect2 = optics.RectangleAperture(position=[12200, 0, -550], zoom=[1, 180, 250])
 
-aper_rect1m = optics.RectangleAperture(pos4d=np.dot(shift_optical_axis, aper_rect1.pos4d))
-aper_rect2m = optics.RectangleAperture(pos4d=np.dot(shift_optical_axis, aper_rect2.pos4d))
+aper_rect1m = optics.RectangleAperture(pos4d=np.dot(shift_optical_axis_12, aper_rect1.pos4d))
+aper_rect2m = optics.RectangleAperture(pos4d=np.dot(shift_optical_axis_12, aper_rect2.pos4d))
 
 aper = optics.MultiAperture(elements=[aper_rect1, aper_rect2])
 aperm = optics.MultiAperture(elements=[aper_rect1m, aper_rect2m])
 
 lens = PerfectLens(focallength=12000., position=entrancepos)
-lensm = PerfectLens(focallength=12000., pos4d=np.dot(shift_optical_axis, lens.pos4d))
+lensm = PerfectLens(focallength=12000., pos4d=np.dot(shift_optical_axis_12, lens.pos4d))
 # Scatter as FWHM ~8 arcsec. Divide by 2.3545 to get Gaussian sigma.
 rms = RadialMirrorScatter(inplanescatter=10. / 2.3545 / 3600 / 180. * np.pi,
                           perpplanescatter=1.5 / 2.345 / 3600. / 180. * np.pi,
@@ -77,7 +99,7 @@ rms = RadialMirrorScatter(inplanescatter=10. / 2.3545 / 3600 / 180. * np.pi,
 
 rmsm = RadialMirrorScatter(inplanescatter=10. / 2.3545 / 3600 / 180. * np.pi,
                            perpplanescatter=1.5 / 2.345 / 3600. / 180. * np.pi,
-                           pos4d=np.dot(shift_optical_axis, rms.pos4d))
+                           pos4d=np.dot(shift_optical_axis_12, rms.pos4d))
 
 
 mirror = Sequence(elements=[lens, rms, mirrorefficiency])
@@ -114,20 +136,24 @@ gratquality = RalfQualityFactor(d=200.e-3, sigma=1.75e-3)
 gratinggrid = {'rowland': rowland, 'd_element': 32., 'x_range': [1e4, 1.4e4],
                'elem_class': CATGrating,
                'elem_args': {'d': 2e-4, 'zoom': [1., 15., 15.], 'orientation': blazemat,
-                             'order_selector': order_selector}
+                             'order_selector': order_selector},
+               'normal_spec': np.array([0, 0., -z_offset_spectra, 1.])
               }
-gas_1 = RectangularGrid(z_range=[300, 800], y_range=[-180, 180], **gratinggrid)
-gas_2 = RectangularGrid(z_range=[-800, -300], y_range=[-180, 180],
+gas_1 = RectangularGrid(z_range=[300 - z_offset_spectra, 800 - z_offset_spectra],
+                        y_range=[-180, 180], **gratinggrid)
+gas_2 = RectangularGrid(z_range=[-800 + z_offset_spectra, -300 - z_offset_spectra],
+                        y_range=[-180, 180],
                         id_num_offset=1000, **gratinggrid)
 gas = Sequence(elements=[gas_1, gas_2, catsupport, catsupportbars, gratquality])
 
 gratinggrid['rowland'] = rowlandm
 gratinggrid['elem_args']['orientation'] = blazematm
-gas_1m = RectangularGrid(z_range=[300, 800], y_range=[-180 + 2 * d, 180 + 2 * d],
-                         normal_spec=np.array([0, 2 * d, 0, 1.]),
+gratinggrid['normal_spec'] = np.array([0, 2 * d, z_offset_spectra, 1.])
+gas_1m = RectangularGrid(z_range=[300 + z_offset_spectra, 800 + z_offset_spectra],
+                         y_range=[-180 + 2 * d, 180 + 2 * d],
                          id_num_offset=2000, **gratinggrid)
-gas_2m = RectangularGrid(z_range=[-800, -300], y_range=[-180 + 2* d, 180 + 2 * d],
-                         normal_spec=np.array([0, 2 * d, 0, 1.]),
+gas_2m = RectangularGrid(z_range=[-800 + z_offset_spectra, -300 + z_offset_spectra],
+                         y_range=[-180 + 2* d, 180 + 2 * d],
                          id_num_offset=3000, **gratinggrid)
 gasm = Sequence(elements=[gas_1m, gas_2m, catsupport, catsupportbars, gratquality])
 
@@ -138,7 +164,8 @@ flatstackargs = {'zoom': [1, 24.576, 12.288],
                  }
 # 500 mu gap between detectors
 
-det = RowlandCircleArray(rowland=rowland, elem_class=marxs.optics.FlatStack,
+det = RowlandCircleArray(rowland=rowland_central,
+                         elem_class=marxs.optics.FlatStack,
                          elem_args=flatstackargs, d_element=49.652,
                          theta=[np.pi - 0.2, np.pi + 0.5])
 
@@ -148,10 +175,23 @@ det = RowlandCircleArray(rowland=rowland, elem_class=marxs.optics.FlatStack,
 projectfp = marxs.analysis.ProjectOntoPlane()
 
 # Place an additional detector on the Rowland circle.
-detcirc = marxs.optics.CircularDetector.from_rowland(rowland, width=20)
-detcirc.loc_coos_name = ['detcirc_phi', 'detcirc_y']
-detcirc.detpix_name = ['detcircpix_x', 'detcircpix_y']
-detcirc.display['opacity'] = 0.1
+detcirc = marxs.optics.CircularDetector.from_rowland(rowland_central, width=20)
+detcirc.loc_coos_name = ['detccent_phi', 'detccent_y']
+detcirc.detpix_name = ['detccentpix_x', 'detccentpix_y']
+detcirc.display['opacity'] = 0.0
+
+# Place an additional detector on the Rowland circle.
+detcirc2 = marxs.optics.CircularDetector.from_rowland(rowlandm, width=20)
+detcirc2.loc_coos_name = ['detc2_phi', 'detc2_y']
+detcirc2.detpix_name = ['detc2pix_x', 'detc2pix_y']
+detcirc2.display['opacity'] = 0.1
+
+# Place an additional detector on the Rowland circle.
+detcirc1 = marxs.optics.CircularDetector.from_rowland(rowland, width=20)
+detcirc1.loc_coos_name = ['detc1_phi', 'detc1_y']
+detcirc1.detpix_name = ['detc1_x', 'detc1_y']
+detcirc1.display['opacity'] = 0.1
+
 
 # Place an additional detector in the focal plane for comparison
 # Detectors are transparent to allow this stuff
@@ -168,11 +208,13 @@ arcusm = Sequence(elements=[aperm, mirrorm, gasm, det, projectfp])
 keeppos = KeepCol('pos')
 keepposm = KeepCol('pos')
 
-arcus_extra_det = Sequence(elements=[aper, mirror, gas, detcirc, det, projectfp, detfp],
-                   postprocess_steps=[keeppos])
+arcus_extra_det = Sequence(elements=[aper, mirror, gas,
+                                     detcirc, detcirc1, detcirc2, det, projectfp, detfp],
+                           postprocess_steps=[keeppos])
 
-arcus_extra_det_m = Sequence(elements=[aperm, mirrorm, gasm, detcirc, det, projectfp, detfp],
-                    postprocess_steps=[keepposm])
+arcus_extra_det_m = Sequence(elements=[aperm, mirrorm, gasm,
+                                       detcirc, detcirc1, detcirc2, det, projectfp, detfp],
+                             postprocess_steps=[keepposm])
 
 # No detector effects - Joern's simulator handles that itself.
 arcus_joern = Sequence(elements=[aper, mirror, gas, detfp])
