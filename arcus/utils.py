3cc77843bfe262a8851569020d1ef316f1b57ceb
hamogu <hgunther@mit.edu>
2020-06-11 15:06:07 -0400

    fix header keywords for fits export

--- arcus/utils.py
+++ arcus/utils.py
@@ -13,11 +13,14 @@ class TagVersion(MarxsElement):
     def __call__(self, photons, *args, **kwargs):
         photons.meta['ARCUSVER'] = (version.version, 'ARCUS code version')
         photons.meta['ARCUSGIT'] = (version.githash, 'Git hash of ARCUS code')
-        photons.meta['ARCUSTIM'] = (version.timestamp, 'Commit time')
-        photons.meta['ARCDATHA'] = (get_git_hash()[:10], 'Git hash of simulation input data')
+        photons.meta['ARCUSTIM'] = (str(version.timestamp), 'Commit time')
+        photons.meta['ARCDATHA'] = (get_git_hash().decode()[:10],
+                                    'Git hash of simulation input data')
         photons.meta['ARCDATGI'] = (string_git_info()[:20], '')
-        photons.meta['ORIGIN'] = (self.origin, 'Institution where file was created')
-        photons.meta['CREATOR'] = (self.creator, 'Person or program creating the file')
+        photons.meta['ORIGIN'] = (self.origin,
+                                  'Institution where file was created')
+        photons.meta['CREATOR'] = (self.creator,
+                                   'Person or program creating the file')
         photons.meta['DATE'] = datetime.now().isoformat()[:10]
         photons.meta['SATELLIT'] = 'ARCUS'
 
