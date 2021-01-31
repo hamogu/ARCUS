395fb3b3ccfda7ce1ba65b3691f40feecbafc941
hamogu <moritz.guenther@gmx.de>
2021-01-31 09:47:11 -0500

    Cylinder is now in marxs

--- arcus/boom.py
+++ arcus/boom.py
@@ -1,63 +1,10 @@
 import numpy as np
-from numpy.core.umath_tests import inner1d
 
 from transforms3d.euler import euler2mat
 from transforms3d.affines import compose
 from marxs.optics import OpticalElement
-from marxs.math import pluecker
-from marxs.math.utils import h2e, norm_vector
 from marxs.simulator import Parallel
-from marxs.math.geometry import Geometry
-
-
-class Cylinder(Geometry):
-
-    loc_coos_name = ['phi', 'z']
-
-    def intersect(self, dir, pos):
-        '''Calculate the intersection point between a ray and the element
-
-        Parameters
-        ----------
-        dir : `numpy.ndarray` of shape (N, 4)
-            homogeneous coordinates of the direction of the ray
-        pos : `numpy.ndarray` of shape (N, 4)
-            homogeneous coordinates of a point on the ray
-
-        Returns
-        -------
-        intersect :  boolean array of length N
-            ``True`` if an intersection point is found.
-        interpos : `numpy.ndarray` of shape (N, 4)
-            homogeneous coordinates of the intersection point. Values are set
-            to ``np.nan`` if no intersection point is found.
-        interpos_local : `numpy.ndarray` of shape (N, 2)
-            y and z coordinates in the coordinate system of the active plane.
-        '''
-        p_rays = pluecker.dir_point2line(h2e(dir), h2e(pos))
-        radius = np.linalg.norm(self.geometry['v_y'])
-        height = np.linalg.norm(self.geometry['v_x'])
-        intersect = np.zeros(pos.shape[0], dtype=bool)
-
-        # ray passes through cylinder caps?
-        for fac in [-1, 1]:
-            cap_midpoint = self.geometry['center'] + fac * self.geometry['v_x']
-            cap_plane = pluecker.point_dir2plane(cap_midpoint,
-                                                 self.geometry['e_x'])
-            interpos = pluecker.intersect_line_plane(p_rays, cap_plane)
-            r = np.linalg.norm(h2e(cap_midpoint) - h2e(interpos), axis=-1)
-            intersect[r < radius]  = True
-
-        # Ray passes through the side of a cylinder
-        # Note that we don't worry about rays parallel to x because those are
-        # tested by passing through the caps already
-        n = norm_vector(np.cross(h2e(self.geometry['e_x']), h2e(dir)))
-        d = np.abs(inner1d(n, h2e(self.geometry['center']) - h2e(pos)))
-        n2 = norm_vector(np.cross(h2e(dir), n))
-        k = inner1d(h2e(pos) - h2e(self.geometry['center']), n2) / inner1d(h2e(self.geometry['e_x']), n2)
-        intersect[(d < radius) & (np.abs(k) < height)] = True
-
-        return intersect, None, None
+from marxs.math.geometry import Cylinder
 
 
 class Rod(OpticalElement):
@@ -92,8 +39,9 @@ class ThreeSidedBoom(Parallel):
     d_longeron = 10.2
     d_batten = 8.
     d_diag = 2.
-    start_bay = 0  # If the bottom of a boom does not contribute to the absorption
-                   # setting start_bay to a larger number can shorten the computation time.
+    # If the bottom of a boom does not contribute to the absorption
+    # setting start_bay to a larger number can shorten the computation time.
+    start_bay = 0
     n_bays = 10
     n_sides = 3
 
@@ -189,7 +137,7 @@ class FourSidedBoom(ThreeSidedBoom):
 
         # diagonal1
         zoom = [self.l_diag / 2., self.d_diag / 2., self.d_diag / 2.]
-        trans = [self.l_longeron / 2., self.l_batten / 2, 0. ]
+        trans = [self.l_longeron / 2., self.l_batten / 2, 0.]
         ang = np.arcsin(self.l_batten / self.l_diag)
         rot = euler2mat(ang, 0, 0, 'syxz')
         pos4d.append(compose(trans, rot, zoom))
