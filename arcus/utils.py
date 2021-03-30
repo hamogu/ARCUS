48531c6ab17e14e01d2cdba687743405d5156be8
hamogu <moritz.guenther@gmx.de>
2021-03-30 13:17:08 -0400

    add plotting utility ordercolor

--- arcus/utils.py
+++ arcus/utils.py
@@ -1,6 +1,7 @@
 import re
 from datetime import datetime
 import subprocess
+import numpy as np
 
 from marxs.base import MarxsElement
 from . import version as vers
@@ -58,3 +59,37 @@ class TagVersion(MarxsElement):
 
 
 tagversion = TagVersion()
+
+
+class OrderColor():
+    '''Consistent color scheme for plots that show multiple different order
+
+    Parameters
+    ---------
+    colormap : string
+        Name of a matplotlib colormap
+    max_order : int
+        Maximum order that this color schme will be used on (i.e. the end of
+        the colorbar will be assigned ot this order)
+    '''
+    def __init__(self, colormap='nipy_spectral', max_order=15):
+        import matplotlib.pyplot as plt
+        cmap = plt.get_cmap(colormap)
+        self.color = cmap(np.linspace(0, 1, abs(max_order)))
+
+    def __call__(self, order):
+        kwargs = {}
+        if order == 0:
+            kwargs['color'] = 'k'
+        else:
+            kwargs['color'] = self.color[abs(order)]
+        if order <= 0:
+            kwargs['linestyle'] = '-'
+        if order > 0:
+            kwargs['linestyle'] = ':'
+        if order == 0:
+            kwargs['lw'] = 4
+            kwargs['alpha'] = .7
+        else:
+            kwargs['lw'] = 2
+        return kwargs
